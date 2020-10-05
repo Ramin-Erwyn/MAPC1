b112ecee348edd61509371aeb129815fb076d58f
RAMIN <r.erwyn@gmail.com>
2020-10-05 21:37:06 +0200

    calories 100g

--- td1/refactor/api/general/Meat.java
+++ td1/refactor/api/general/Meat.java
@@ -8,49 +8,51 @@ public class Meat implements FoodProduct {
         BEEF, WHITEFISH;
 
 
-        public double price() {
+
+        public Double price(){
             double rtr=0;
             if(this == WHITEFISH) rtr= 6;
             if (this ==BEEF) rtr =4;
             else return rtr;
         }
 
-        public double caloriesPer100grams() {
-            double calorie = 0.0;
+        public int caloriesPer100grams() {
+            double calorie = 0;
             if(this == BEEF) return 200;
             if(this == WHITEFISH) return 170;
-            else return calorie;
-    }
+            else return (int) calorie;
+        }
+
 
     private MeatType type;
     private double weight;
 
-    public Meat(MeatType type, double weight) {
+     MeatType(MeatType type, double weight) {
         this.type = type;
         this.weight = weight;
     }
 
-    @Override
-    public double price() {
+
+    public Double price() {
         return type.price() * weight / 100;
     }
 
-    @Override
+
     public double caloriesPer100grams() {
         return this.type.caloriesPer100grams();
     }
 
-    @Override
+
     public double calories() {
         return this.caloriesPer100grams() * this.weight() / 100;
     }
 
-    @Override
+
     public double weight() {
         return weight;
     }
 
-    @Override
+
     public String toString() {
         return String.format("%s (%.0fg) -- %.2f€", type, weight(), price());
     }
